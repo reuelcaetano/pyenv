f605faa43eb27768f574a7559c1811fffcc1ffed
Reuel Caetano <reuelbarbosacaetano@gmail.com>
2024-06-09 13:55:46 -0300

    Input component

---
+++ pyenv/gui.py
@@ -0,0 +1,16 @@
+def inputGUI(typeof: str) -> str:
+    """
+    Cria um componente de input.
+
+    Parameters:
+        typeof: Tipo do input
+
+    Returns:
+        O conteúdo do input
+    
+    Examples:
+        >>> inputGUI('text')
+        ''
+    """
+    data = ''
+    return data
